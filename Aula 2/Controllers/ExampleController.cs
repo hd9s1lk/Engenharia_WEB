d265230cf83d779181c39369e4041c15d0da02c8
hd9s1lk <hd9business@gmail.com>
2024-09-27 10:20:34 +0100

    Aula_2

---
+++ Aula 2/Controllers/ExampleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula2.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public IActionResult Index()  //abrir view clicar no direito do rato no "Index" e adicionar view
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormCollection formData)    //cada view tem um controller, neste caso controller do index
+        {
+            ViewData["text_inserted"] = formData["name"]; //dados
+            ViewData["other_Text_inserted"] = formData["age"];
+
+            return View("Index2");
+        }
+    }
+}
---
+++ Aula 2/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aula2.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [Range(18,100,ErrorMessage ="{0} must be between {1} and {2}")]
+        public int Age { get; set; }
+    }
+}
